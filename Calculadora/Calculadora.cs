2c77a62606f9f03c8617ba502afe91f48e354576
Rafael Souza <rafaelsouza2017a@gmail.com>
2025-09-29 13:33:48 -0300

    feat: Processa fila de operações com histórico e stack de resultados

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,32 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Calculadora
 {
     public class Calculadora
     {
-        
+        private Stack<Operacoes> historicoOperacoes;
+        public Calculadora()
+        {
+            historicoOperacoes = new Stack<Operacoes>();
+        }
         public Operacoes calcular(Operacoes operacao)
         {
-            switch(operacao.operador)
+            switch (operacao.operador)
             {
-                case '+': operacao.resultado= soma(operacao);break;
-                case '-': operacao.resultado = subtracao(operacao);break;
-                case '*': operacao.resultado = multiplicacao(operacao);break;
+                case '+': operacao.resultado = soma(operacao); break;
+                case '-': operacao.resultado = subtracao(operacao); break;
+                case '*': operacao.resultado = multiplicacao(operacao); break;
+                case '/': operacao.resultado = divisao(operacao); break; // Adicionado a operação de divisão
                 default: operacao.resultado = 0; break;
             }
+            AdicionarAoHistorico(operacao);
             return operacao;
         }
-        public int soma(Operacoes operacao)
+        public long soma(Operacoes operacao)
         {
             return operacao.valorA + operacao.valorB;
         }
-        public int subtracao(Operacoes operacao)
+        public long subtracao(Operacoes operacao)
         {
             return operacao.valorA - operacao.valorB;
         }
-        public int multiplicacao(Operacoes operacao)
+        public long multiplicacao(Operacoes operacao)
         {
             return operacao.valorA * operacao.valorB;
         }
-       
+        public decimal divisao(Operacoes operacao)
+        {
+            if (operacao.valorB == 0)
+            {
+                throw new DivideByZeroException("Não é possível dividir por zero");
+            }
+            return (decimal)operacao.valorA / operacao.valorB;
+        }
+        private void AdicionarAoHistorico(Operacoes operacao)
+        {
+            historicoOperacoes.Push(operacao);
+        }
+        public IEnumerable<Operacoes> Historico()
+        {
+            return historicoOperacoes;
+        }
+
     }
 }
